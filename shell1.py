64c903f39e7ce50ec6a88e9ba7e2f789ba377941
Landon Jamieson <landonjamieson@gmail.com>
2016-10-04 18:35:10 -0600

    reworked things, need to make some changes to predict.

--- shell1.py
+++ shell1.py
@@ -7,10 +7,15 @@ import sys
 pd.options.mode.chained_assignment = None
 
 
+class Node():
+    def __init__(self, feature_name, child_nodes):
+        self.feature_name = feature_name
+        self.child_nodes = child_nodes
+
+
 class HardCodedClassifier:
-    classes = None
-    target = None
-    data = None
+    def __init__(self):
+        self.classes = self.target = self.data = None
 
     def train(self, train_data, train_target):
         print("And thus, the Machine was trained.")
@@ -29,42 +34,72 @@ class HardCodedClassifier:
 
 
 class DecisionTreeClassifier(HardCodedClassifier):
-    def info_gain(self):
+    def __init__(self):
+        super(HardCodedClassifier).__init__()
+        self.tree = None
 
-        # splits the data into columns on each attribute
-        columns = []
-        for i in range(self.data.shape[1]):
-            columns.append(self.data[:, i])
+    def train(self, train_data, train_target):
+        self.data = train_data
+        self.target = train_target
+        self.tree = self.make_tree(range(train_data.shape[1]), range(train_target.shape[0]))
 
-        entropy = []
-        for col in columns:
-            entropy.append(sum(list(map(lambda val: -val * np.log2(val) if val != 0 else 0, col))))
+    def make_tree(self, features_left, indices):
+        # get list of each class result for the indices
+        classes_list = list(map(lambda i: self.target[i], indices))
 
-        feature = np.argmax(entropy)
-        values = np.unique(columns[feature])
-        print(values)
+        # base case if there is only one unique class in list
+        if np.unique(classes_list).size == 1:
+            return classes_list[0]
 
+        # base case if there are no more features
+        if len(features_left) == 0:
+            return co(classes_list).most_common(1)[0][0]
+
+        # which feature has the lowest entropy
+        best_feature = self.best_info_gain(features_left, indices)
+        values_of_feature = np.unique(list(map(lambda i: self.data[:, best_feature][i], indices)))
+        value_indices = list(map(
+            lambda val: [ind for ind in indices if self.data[:, best_feature][ind] == val], values_of_feature))
+        remaining = [i for i in features_left if i != features_left[best_feature]]
+        return Node(features_left[best_feature],
+                    {x: self.make_tree(remaining, y) for x in values_of_feature for y in value_indices})
+
+    def best_info_gain(self, features, indices):
+        """
+        This function will calculate figure out which feature has the most info gain
+        :param features: The features you have to look between
+        :param indices: Which spots we are looking at right now
+        :return: The index of the feature with the best info gain or least entropy
+        """
+        return np.argmin(list(map(lambda feature: self.entropy_of_feature(feature, indices), features)))
+
+    def entropy_of_feature(self, feature, indices):
+        # get the possible values for the feature
+        values_of_feature = np.unique(self.data[:, feature])
+
+        # get the indices of each of those values
+        value_indices = list(map(
+            lambda val: [ind for ind in indices if self.data[:, feature][ind] == val], values_of_feature))
         cnt = co()
-        class_counts = []
-        for val in values:
-            for index in [i for i, x in enumerate(self.target) if columns[feature][i] == val]:
-                cnt[self.target[index]] += 1
-            class_counts.append(list(cnt.values()))
-            print(list(cnt.values()))
-            cnt.clear()
+        # total number for weighted average
+        num_total = sum(map(lambda val_i: len(val_i), value_indices))
+        total_entropy = 0
 
-        print(class_counts)
-        # for i in values[1]:
-        #     class_count.append(self.target[i])
-        # feature_count = np.zeros(len(values[0]))
-        # print(co(class_count))
+        for vi in value_indices:
+            num = len(vi)
+            for i in vi:
+                cnt[self.target[i]] += 1
+            total_entropy += (num / num_total) * sum(map(
+                lambda c: -cnt[c] / num * np.log2(cnt[c] / num) if cnt[c] != 0 else 0, self.classes))
+            cnt.clear()
+        return total_entropy
 
 
 class KGNClassifier(HardCodedClassifier):
     """K-GoodNeighbors, a nearest neighbors algorithm"""
-    k = None
-    mean = None
-    std = None
+    def __init__(self):
+        super(HardCodedClassifier).__init__()
+        self.k = self.mean = self.std = None
 
     def knn(self, inputs):
         distances = np.sum((self.data - inputs)**2, axis=1)
@@ -135,7 +170,7 @@ def get_classifier():
     return KGNClassifier() if which == 2 else HardCodedClassifier()
 
 
-def get_dataset():
+def get_dataset(convert_nominal=True):
     which = int(input("Please choose a Dataset:\n1 - Iris\n2 - Cars\n>> "))
     if which == 1:
         iris = ds.load_iris()
@@ -144,26 +179,36 @@ def get_dataset():
         my_read_in = pd.read_csv("car.csv", dtype=str,
                                  names=["buying", "maint", "doors", "persons", "lug_boot", "safety", "target"])
         car_data = my_read_in.ix[:, :-1]
-        car_target = my_read_in.target.replace("unacc", 0).replace("acc", 1).replace("good", 2).replace("vgood", 3)
-        car_data.buying = car_data.buying.replace("low", 1).replace("med", 2).replace("high", 3).replace("vhigh", 4)
-        car_data.maint = car_data.maint.replace("low", 1).replace("med", 2).replace("high", 3).replace("vhigh", 4)
-        car_data.doors = car_data.doors.replace("2", 2).replace("3", 3).replace("4", 4).replace("5more", 5)
-        car_data.persons = car_data.persons.replace("2", 2).replace("4", 4).replace("more", 6)
-        car_data.lug_boot = car_data.lug_boot.replace("small", 1).replace("med", 2).replace("big", 3)
-        car_data.safety = car_data.safety.replace("low", 1).replace("med", 2).replace("high", 3)
+        car_target = my_read_in.target
+        if convert_nominal:
+            car_target = car_target.replace("unacc", 0).replace("acc", 1).replace("good", 2).replace("vgood", 3)
+            car_data.buying = car_data.buying.replace("low", 1).replace("med", 2).replace("high", 3).replace("vhigh", 4)
+            car_data.maint = car_data.maint.replace("low", 1).replace("med", 2).replace("high", 3).replace("vhigh", 4)
+            car_data.doors = car_data.doors.replace("2", 2).replace("3", 3).replace("4", 4).replace("5more", 5)
+            car_data.persons = car_data.persons.replace("2", 2).replace("4", 4).replace("more", 6)
+            car_data.lug_boot = car_data.lug_boot.replace("small", 1).replace("med", 2).replace("big", 3)
+            car_data.safety = car_data.safety.replace("low", 1).replace("med", 2).replace("high", 3)
 
         return car_data.values, car_target.values, ["unacc", "acc", "good", "vgood"]
 
 
 def main(argv):
     # process_data()
-    d, t, ta = get_dataset()
+    d, t, ta = get_dataset(False)
 
     my_classifier = DecisionTreeClassifier()
     train, test, t_target, test_target = get_split_size(d, t, True)
-    my_classifier.data = train
-    my_classifier.target = t_target
-    my_classifier.info_gain()
+    my_classifier.set_classes(ta)
+    my_classifier.train(train, t_target)
+    print(my_classifier.tree.feature_name, my_classifier.tree.child_nodes,
+          my_classifier.tree.child_nodes['low'].feature_name, my_classifier.tree.child_nodes['low'].child_nodes,
+          my_classifier.tree.child_nodes['low'].child_nodes['more'].feature_name,
+          my_classifier.tree.child_nodes['low'].child_nodes['more'].child_nodes,
+          my_classifier.tree.child_nodes['low'].child_nodes['more'].child_nodes['low'].feature_name,
+          my_classifier.tree.child_nodes['low'].child_nodes['more'].child_nodes['low'].child_nodes,
+          my_classifier.tree.child_nodes['low'].child_nodes['more'].child_nodes['low'].child_nodes['vhigh'].feature_name,
+          my_classifier.tree.child_nodes['low'].child_nodes['more'].child_nodes['low'].child_nodes['vhigh'].child_nodes,
+          sep='\n')
 
 
 if __name__ == '__main__':
