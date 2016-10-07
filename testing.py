b88ca9f54282ee339b33eadaa88eada18fea2475
Cam Parra <caeparra@gmail.com>
2016-10-07 11:08:13 -0600

    adding storage for the edges

--- testing.py
+++ testing.py
@@ -5,10 +5,14 @@ from scipy.stats import binned_statistic_2d as bs2
 
 iris = ds.load_iris()
 d =[]
+ed = []
 for items in range(iris.data.shape[1]):
     hist, edges = np.histogram(iris.data[:, items], bins=3)
+    # this just stores the values we used to bin things
+    ed.append(edges.tolist())
     d.append(np.digitize(iris.data[:, items], edges).tolist())
 de = []
+print(ed)
 for i in range(len(d[0])):
     de.append(list(map(lambda x: x[i], d)))
 print(np.asarray(de))
