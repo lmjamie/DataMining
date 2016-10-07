b5a12faf5171f359b2f813ae02e2204a65cfe3de
Cam Parra <caeparra@gmail.com>
2016-10-07 09:38:53 -0600

    just more stuff

--- testing.py
+++ testing.py
@@ -1,8 +1,12 @@
 from sklearn import datasets as ds
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import binned_statistic_2d as bs2
 
 iris = ds.load_iris()
-plt.hist(iris.data, bins='scott')
-plt.title("test")
-plt.show()
+cancer = ds.load_breast_cancer()
+print(list(map(list, cancer.data)))
+# plt.hist(iris.data, bins=ValueError: too many values to unpack (expected 2)'scott')
+# plt.title("test")
+#
+# plt.show()
